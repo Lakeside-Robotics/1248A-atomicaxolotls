b67cc370a0607901160e35e65ed225d42619bba0
noahl25 <97693037+noahl25@users.noreply.github.com>
2023-12-07 22:40:03 -0800

    Update vex.h

--- include/vex.h
+++ include/vex.h
@@ -22,14 +22,13 @@
 
 namespace robot {
 
-  inline const std::pair<uint8_t, uint8_t> gearRatio{3, 4}; 
   inline const double wheelGearCircumfrenceIN = 1; //replace with actual circumfrence
 
   template <typename T, typename = typename std::enable_if< (std::is_same< T, vex::motor>::value || std::is_same<T, vex::motor_group>::value) >::type >
-  inline void spinFor(const T& motorToSpin, vex::distanceUnits dstType, double dst, vex::velocityUnits velType, double vel) {
+  inline void spinFor(const T& motorToSpin, vex::distanceUnits dstType, double dst, vex::velocityUnits velType, double vel std::pair<uint8_t, uint8_t> gearRatio = {1, 1}) {
 
     int dstIN = (dstType == vex::distanceUnits::cm) ? dst * 0.393701 : (dstType == vex::distanceUnits::mm) ? dst * 0.0393701 : dst;
-    int rot = 360 * (dstIN / wheelGearCircumfrenceIN);
+    int rot = 360 * ((dstIN / wheelGearCircumfrenceIN) * gearRatio);
     motorToSpin.spinFor(vex::directionType::fwd, rot, vex::rotationUnits::deg, vel, velType);
     
   }
